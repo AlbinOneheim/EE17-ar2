f3afffa98a1f43a1f68a998cab81c4dfcbf50666
elev <elev@8FY06H2.learnet.se>
2019-10-15 12:53:18 +0200

    glömde lägga up den på förra lektionen

--- kapitel-5/uppgift-5-5.php
+++ kapitel-5/uppgift-5-5.php
@@ -38,7 +38,7 @@
 
             if ($losen) { 
                 /* Ska innehålla mins en stor boksatav */
-                $versaler = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"];
+                $versaler = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W","X", "Y", "Z", "Å", "Ä", "Ö"];
                 foreach ($versaler as $tecken){
                     $pos = strpos($losen, $tecken);
                     if ($pos !== false){
