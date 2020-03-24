19934f62c67d909bb3fd32bfd25abb948eb64d75
AlbinOneheim <albin.onehein@gmail.com>
2020-03-24 14:35:22 +0100

    tränat på tilesheet, for loop, switch, array

--- javascript/nyckelpiga/nyckelpiga.js
+++ javascript/nyckelpiga/nyckelpiga.js
@@ -1,45 +1,93 @@
+/* Element vi jobbar med */
 const eCanvas = document.querySelector("canvas");
 
+/* Ställ in bredd och höjd */
 eCanvas.width = 800;
 eCanvas.height = 600;
 
 /* Globala variabler */
 var ctx = eCanvas.getContext("2d");
 var piga = {
-    x: 0,
-    y: 0,
+    rad: 0,
+    kol: 0,
     rot: 0,
     bild: new Image()
 }
+var karta = [
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+    [1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0],
+    [0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+];
 
 /* Nyckelpigans startläga */
-piga.x = eCanvas.width / 2;
-piga.y = eCanvas.height / 2;
+piga.rad = 0;
+piga.kol = 1;
 piga.bild.src = "./nyckelpiga.png";
+
+var tileSheet = new Image();
+tileSheet.src = "./tilesheet.png";
 
 /* Rita ut nyckelpigan */
 function ritaPiga() {
     ctx.save();
-    ctx.translate(piga.x, piga.y);
+    ctx.translate(piga.kol * 50 + 25, piga.rad * 50 + 25);
+    ctx.rotate(piga.rot);
     ctx.drawImage(piga.bild, -25, -25, 50, 50);
     ctx.restore();
+}
+
+/* Rita ut kartan */
+function ritaKarta() {
+    /* Gå igenom varje rad */
+    for (var rad = 0; rad < karta.length; rad++) {
+        /* Gå igenom varje kolumn */
+        for (var kol = 0; kol < karta[rad].length; kol++) {
+            if (karta[rad][kol] == 1) {
+                /* Rita ut en svart fyrkant 50x50 px */
+                var rutaPos = karta[rad][kol] * 32;
+                ctx.drawImage(tileSheet, rutaPos, 0, 32, 32, kol * 50, rad * 50, 50, 50);
+            }
+        }
+    }
 }
 
 /* Lyssna på pil-tangenterna */
 window.addEventListener("keydown", function (e) {
     switch (e.key) {
         case "ArrowRight":
-            piga.x += 5
+            if (karta[piga.rad][piga.kol + 1] == 0) {
+                piga.kol++; 
+            }
+            
             piga.rot = 90 * (Math.PI / 180);
             break;
         case "ArrowLeft":
-            piga.x -= 5
+            if (karta[piga.rad][piga.kol - 1] == 0) {
+                piga.kol--;
+            }
+            piga.rot = -90 * (Math.PI / 180);
             break;
         case "ArrowUp":
-            piga.y -= 5
+            if (karta[piga.rad - 1][piga.kol] == 0) {
+                piga.rad--; 
+            }
+            piga.rot = 0;
             break;
         case "ArrowDown":
-            piga.y += 5
+            if (karta[piga.rad + 1][piga.kol] == 0) {
+                piga.rad++; 
+            }
+            
+            piga.rot = Math.PI;
             break;
     }
 });
@@ -48,6 +96,8 @@ window.addEventListener("keydown", function (e) {
 function gameLoop() {
     /* Rensa canvas */
     ctx.clearRect(0, 0, eCanvas.width, eCanvas.height);
+
+    ritaKarta();
     ritaPiga();
 
     requestAnimationFrame(gameLoop);
