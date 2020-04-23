5136a99a679a4c826d6837eaf6476d0f4df0965e
AlbinOneheim <42640762+AlbinOneheim@users.noreply.github.com>
2020-04-23 16:12:05 +0200

    tränat på att plocka ut information från JSON

---
+++ api/chucknorris/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chuck Norris skämt</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img src="" alt="">
+    <div class="kontainer">
+        <h1>En Chuck norris skämt</h1>
+        <?php
+        /* Tjänsten */
+        $url = "https://api.chucknorris.io/jokes/random";
+        
+        $json = file_get_contents($url);
+        
+        /* Avkoda JSON */
+        $jsonData = json_decode($json);
+    
+        /* plocka ut skämtet */
+        $skämtet = $jsonData->value;
+
+        /* Plocka ut bilden */
+        $bilden = $jsonData->icon_url;
+        
+        /* Skriv ut skämtet och bilden*/
+        echo "<blockquote>
+            $skämtet
+            <footer>Chuck Norris</footer>
+            <img src=\"$bilden\" alt=\"Chuck norris\">
+            </blockquote>";
+
+        
+        ?>
+    </div>
+</body>
+</html>
---
+++ api/owm/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vädret</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <?php
+        /* Api-nyckel */
+        $api = "22ee1d58f7adae08ee71fa7c0bd24481";
+
+        /* Stad */
+        $stad = "Stockholm";
+
+        echo "<h1>Vädret idag i $stad</h1>";
+
+        /* Tjänsten */
+        $url = "https://api.openweathermap.org/data/2.5/weather?q=$stad&appid=$api&units=metric";
+        
+        $json = file_get_contents($url);
+        
+        /* Avkoda JSON */
+        $jsonData = json_decode($json);
+        
+        /* Plocka ut koordinaterna */
+        $koordinater = $jsonData->coord;
+        $lon = $koordinater->lon;
+        $lat = $koordinater->lat;
+
+        echo "<p>Koordinater (lat, lon): $lat, $lon</p>";
+
+        /* Plocka ut vädret */
+        $väder = $jsonData->weather;
+        $himlen = $väder[0]->description;
+        $ikon = $väder[0]->icon;
+
+        echo "<p>Himlen är: $himlen</p>";
+        echo "<p><img src=\"http://openweathermap.org/img/wn/$ikon@2x.png\"></p>";
+
+        /* Plocka ut temperaturen i celsius*/
+        $temp = $jsonData->main->temp;
+        echo "<p>Temperaturen just nu är: $temp&deg</p>";
+
+        /* Plocka ut vindhastigeten */
+        $vind = $jsonData->wind->speed;
+        echo "<p>Vindhastigeten just nu är: $vind m/s</p>";
+        ?>
+    </div>
+</body>
+</html>
